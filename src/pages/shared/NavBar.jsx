b6af87331d628eb158f32693e45ef39b103c2d10
akashislam1 <m.naimul110@gmail.com>
2023-05-17 23:02:42 +0600

    change navbar

--- src/pages/shared/NavBar.jsx
+++ src/pages/shared/NavBar.jsx
@@ -1,72 +1,110 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaList, FaTimes } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
 const NavBar = () => {
-  const navItems = (
-    <>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      {/* {user ? (
-        <>
-          <li>
-            <Link to="/bookings"> My Bookings</Link>
-          </li>
-          <button onClick={logOut}>Sign Out</button>
-        </>
-      ) : (
-        <li>
-          <Link to="/Login">Login</Link>
-        </li>
-      )} */}
-      <li>
-        <Link>All Toys</Link>
-      </li>
-      <li>
-        <Link>Blogs</Link>
-      </li>
-    </>
-  );
+  const [menu, setMenu] = useState(false);
+
   return (
-    <div className="navbar bg-[#4acdd5] text-white rounded-md font-bold">
-      <div className="navbar-start">
-        <div className="dropdown ">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="relative shadow-md rounded-md bg-[#4acdd5] text-white font-bold">
+      <div className="w-full md:w-5/6 mx-auto hidden md:block">
+        <nav className=" z-50 p-4 rounded-md flex justify-between items-center ">
+          <h2>
+            <Link to="/" className="flex items-center gap-2">
+              <img src={logo} alt="" />
+              <p>Kids Wonders</p>
+            </Link>
+          </h2>
+          <div className="flex items-center gap-3 font-semibold ">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "default")}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow  w-52 bg-[#4acdd5] text-white "
-          >
-            {navItems}
+              Home
+            </NavLink>
+            <NavLink
+              to="/blogs"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Blog
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+      <div className="p-4 md:hidden shadow-md rounded-md flex justify-between items-center">
+        <h2 className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2">
+            <img src={logo} alt="" />
+            <p>Kids Wonders</p>
+          </Link>
+        </h2>
+        <div onClick={() => setMenu(!menu)}>
+          {menu ? (
+            <FaTimes className="text-xl text-white"></FaTimes>
+          ) : (
+            <FaList className="text-xl text-white"></FaList>
+          )}
+        </div>
+      </div>
+
+      {menu && (
+        <div className="z-50 absolute inset-y-16 left-0 w-full max-h-min bg-[#4acdd5] p-5 text-white">
+          <ul className="text-center font-bold">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "default")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/blogs"
+                className={({ isActive }) => (isActive ? "active" : "default")}
+              >
+                Blog
+              </NavLink>
+            </li>
+            {/* <li>
+              {user ? (
+                <div className="flex justify-center items-center flex-col gap-3">
+                  <Link onClick={logOut}>Sign Out</Link>
+                  {user.photoURL ? (
+                    <div className="group flex relative cursor-pointer">
+                      <img
+                        className="w-8 h-8 rounded-full "
+                        src={user.photoURL}
+                        alt=""
+                      />
+                      <span className="group-hover:opacity-100 transition-opacity bg-gray-800 text-sm text-gray-100 rounded-md absolute left-1/2  -translate-x-1/2 translate-y-full opacity-0 mt-5 mx-auto py-1 px-2 w-28 text-center">
+                        {user.displayName}
+                      </span>
+                    </div>
+                  ) : (
+                    <div className="group flex relative cursor-pointer">
+                      <FaUserCircle className="w-8 h-8"></FaUserCircle>
+                      <span className="group-hover:opacity-100 transition-opacity bg-gray-800 text-sm text-gray-100 rounded-md absolute left-1/2  -translate-x-1/2 translate-y-full opacity-0 mt-5 mx-auto py-1 px-2 w-28 text-center">
+                        {user.displayName}
+                      </span>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    isActive ? "active" : "default"
+                  }
+                >
+                  Login
+                </NavLink>
+              )}
+            </li> */}
           </ul>
         </div>
-        <Link to="/" className=" normal-case text-xl flex items-center gap-2">
-          <img className="w-24 md:w-14 " src={logo} alt="" />
-          <p className="font-bold ">Kids Wonders</p>
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex ">
-        <ul className="menu menu-horizontal px-1 ">{navItems}</ul>
-      </div>
-      <div className="navbar-end ">
-        <div className="hidden md:block">
-          <button className="btn  btn-accent text-white ">Login</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
