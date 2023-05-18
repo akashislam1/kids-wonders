4c604b8181121d438c25824173a1a679cf3a05eb
akashislam1 <m.naimul110@gmail.com>
2023-05-18 14:20:59 +0600

    loade filter data by category and show ui

--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,30 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import TabsByCategory from "./TabsByCategory";
 
 const ShopByCategory = () => {
+  const [toys, setToys] = useState([]);
   const [activeTab, setActiveTab] = useState("sports-car");
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/toys/sub-category?subCategory=${activeTab}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setToys(data);
+      });
+  }, [activeTab]);
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
   };
-  console.log(activeTab);
+  // console.log(toys);
+
   return (
-    <div className="p-4">
+    <div className="p-4 w-full md:w-2/3 mx-auto ">
+      <h2 className="text-center font-bold text-4xl underline mt-5 mb-8">
+        Shop By Category
+      </h2>
       <Tabs>
-        <TabList>
+        <TabList className="flex gap-2 text-xs md:text-xl font-bold">
           <Tab onClick={() => handleTabClick("sports-car")}>Sports car</Tab>
           <Tab onClick={() => handleTabClick("police-car")}>Police car</Tab>
           <Tab onClick={() => handleTabClick("fire-truck")}>Fire truck</Tab>
         </TabList>
 
         <TabPanel>
-          <h2>Any content 1</h2>
+          <div className="flex flex-col md:flex-row md:gap-8 overflow-x-hidden overflow-y-hidden">
+            {toys?.map((toy) => (
+              <TabsByCategory key={toy._id} toy={toy}></TabsByCategory>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          <div className="flex flex-col md:flex-row md:gap-8 overflow-x-hidden overflow-y-hidden">
+            {toys?.map((toy) => (
+              <TabsByCategory key={toy._id} toy={toy}></TabsByCategory>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 3</h2>
+          <div className="flex flex-col md:flex-row md:gap-8 overflow-x-hidden overflow-y-hidden">
+            {toys?.map((toy) => (
+              <TabsByCategory key={toy._id} toy={toy}></TabsByCategory>
+            ))}
+          </div>
         </TabPanel>
       </Tabs>
     </div>
---
+++ src/pages/Home/ShopByCategory/TabsByCategory.jsx
@@ -0,0 +1,38 @@
+import Rating from "react-rating";
+import { FaRegStar, FaStar } from "react-icons/fa";
+import { useEffect } from "react";
+import Aos from "aos";
+
+const TabsByCategory = ({ toy }) => {
+  const { toy_name, picture, price, rating } = toy;
+  useEffect(() => {
+    Aos.init();
+  }, []);
+  return (
+    <div
+      data-aos="fade-down"
+      data-aos-duration="1500"
+      className="my-4 rounded-md border border-[#4acdd5]  flex flex-col justify-between gap-3 p-5 hover:shadow-xl"
+    >
+      <img className="w-64 h-64" src={picture} alt="" />
+      <div className="text-center border-t-2">
+        <h2 className="font-bold text-xl my-4">Name : {toy_name}</h2>
+        <p>Price : ${price}</p>
+        <p className="mt-2 mb-5">
+          <Rating
+            readonly
+            placeholderRating={rating}
+            emptySymbol={<FaRegStar></FaRegStar>}
+            placeholderSymbol={<FaStar className="text-yellow-400"></FaStar>}
+            fullSymbol={<FaStar></FaStar>}
+          ></Rating>
+        </p>
+        <button className="font-bold text-white bg-[#4acdd5] text-center  rounded  hover:bg-white hover:text-[#4acdd5] border hover:border-[#4acdd5] duration-200 py-2 px-4  focus:outline-none focus:shadow-outline ">
+          View Details
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TabsByCategory;
