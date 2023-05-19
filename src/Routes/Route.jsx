de724026b425ed1a484aaf4fbe8bb98ef6d4ce56
akashislam1 <m.naimul110@gmail.com>
2023-05-19 10:14:51 +0600

    change modal and create update route

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -9,6 +9,7 @@ import AllToys from "../pages/AllToys/AllToys";
 import AddToy from "../pages/AddToyPage/AddToy";
 import PrivateRoute from "./PrivateRoute";
 import MyToys from "../pages/MyToys/MyToys";
+import UpdateToyDetails from "../pages/MyToys/UpdateToyDetails";
 
 const router = createBrowserRouter([
   {
@@ -50,6 +51,10 @@ const router = createBrowserRouter([
         ),
         loader: ({ params }) =>
           fetch(`http://localhost:5000/toys/${params.id}`),
+      },
+      {
+        path: "/updateToyDetails/:id",
+        element: <UpdateToyDetails></UpdateToyDetails>,
       },
       {
         path: "/signup",
--- src/pages/MyToys/MyToyTableRow.jsx
+++ src/pages/MyToys/MyToyTableRow.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import UpdateModal from "./UpdateModal";
 
 const MyToyTableRow = ({ toy, index }) => {
   const { _id, toy_name, price, seller_name, subCategory, available_quantity } =
@@ -16,9 +15,9 @@ const MyToyTableRow = ({ toy, index }) => {
         {available_quantity}
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
-        {/* <Link to={`/toy-details/${_id}`}> */}
-        <UpdateModal toy={toy}></UpdateModal>
-        {/* </Link> */}
+        <Link to={`/updateToyDetails/${_id}`}>
+          <button>Update</button>
+        </Link>
         <button className="font-bold text-white bg-[#f10404] text-center  rounded-full  hover:bg-white hover:text-[#f10404] border hover:border-[#f10404] duration-200 py-2 px-4  focus:outline-none focus:shadow-outline">
           X
         </button>
