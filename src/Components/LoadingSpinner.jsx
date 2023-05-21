4fcc1744fc84a1b6b8d794601c496c9a2f4e940a
akashislam1 <m.naimul110@gmail.com>
2023-05-21 21:11:42 +0600

    change width height loader

--- src/Components/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-[calc(100vh-68px)] ">
-      <div className="w-10 h-10 border-8 border-dashed rounded-full animate-spin mt-5 border-[#4acdd5] "></div>
+      <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin mt-5 border-[#4acdd5] "></div>
     </div>
   );
 };
